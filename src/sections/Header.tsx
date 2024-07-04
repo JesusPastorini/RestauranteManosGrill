1009c87f43d56521b19894c9e6dfa6fcee9345c4
JuniorPastorini <juniorpastorini_@hotmail.com>
2024-07-04 03:29:30 -0300

    borda ifood

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { FaBars } from 'react-icons/fa';
 import logo from '../assets/logoManos.png';
 import ifoodIcon from '../assets/ifood.svg';
+import { FaFacebook, FaInstagram, FaWhatsapp } from 'react-icons/fa';
 
 const Header: React.FC = () => {
     const [isScrolled, setIsScrolled] = useState(false);
@@ -36,18 +37,29 @@ const Header: React.FC = () => {
                 <div className="hidden md:block absolute left-1/2 transform -translate-x-1/2">
                     <a href="https://www.ifood.com.br/delivery/novo-hamburgo-rs/restaurante-manos-grill-centro/02ddffd4-2424-4f45-9b56-479ad19e4b4b?prato=e46d055d-f4c6-49a2-a267-c3ccfc2dd231" target="_blank" rel="noopener noreferrer" className="relative group">
                         <img src={ifoodIcon} alt="iFood" className="w-20 h-20 transition-transform transform group-hover:scale-105" />
-                        <div className="absolute inset-0 top-1/2 left-1/2 w-24 h-16 rounded-lg border-4 border-orange-300 opacity-0 group-hover:opacity-100 transition-opacity duration-300 transform -translate-x-1/2 -translate-y-1/2"></div>
+                        <div className="absolute inset-0 top-9 left-1/2 w-24 h-16 rounded-full border-4 border-orange-300 opacity-0 group-hover:opacity-100 transition-opacity duration-300 transform -translate-x-1/2 -translate-y-1/2"></div>
                     </a>
                 </div>
                 <nav className="flex flex-grow justify-end items-center">
+                    <div className="md:hidden flex space-x-4 mr-2">
+                        <a href="https://www.facebook.com/rest.manosgrill/?locale=pt_BR" target="_blank" rel="noopener noreferrer">
+                            <FaFacebook size={32} className="text-orange-300" />
+                        </a>
+                        <a href="https://www.instagram.com/restaurantemanosgrill/" target="_blank" rel="noopener noreferrer">
+                            <FaInstagram size={32} className="text-orange-300" />
+                        </a>
+                        <a href="https://api.whatsapp.com/send?phone=%2B555135684494&context=ARAdqdePNYZumfQYCtFscWo720rKsP3XrE-HYyaG78gBE3kFBYh78IFeYu2wdkCNjet0G79VzwgEq96BOt-S6onjZ6A-pDO8PaFUbRdzxmc3c3bD7AahfZqx9Zk9dADhVTib0ctGWn2bRVG53gZerBTXSQ&source=FB_Page&app=facebook&entry_point=page_cta&fbclid=IwZXh0bgNhZW0CMTAAAR1PgwTB6zRBDQzcY7mF05opw9C8U-Qhqm_mrjlO9ujvh5poGzqX3CSbUS4_aem_w7rh5yCUNM0PXaQWukKBFA" target="_blank" rel="noopener noreferrer">
+                            <FaWhatsapp size={32} className="text-orange-300" />
+                        </a>
+                    </div>
                     <ul className="hidden md:flex space-x-4 text-xl">
-                        <li><a href="#Buffet" className="text-orange-300 hover:text-gray-50 hover:border-b-2 border-orange-300">Buffet</a></li>
-                        <li><a href="#services" className="text-orange-300 hover:text-gray-50 hover:border-b-2 border-orange-300">Serviços</a></li>
-                        <li><a href="#location" className="text-orange-300 hover:text-gray-50 hover:border-b-2 border-orange-300">Localização</a></li>
-                        <li><a href="#faleconosco" className="text-orange-300 hover:text-gray-50 hover:border-b-2 border-orange-300">Fale Conosco</a></li>
+                        <li><a href="#Buffet" className="text-orange-300 hover:text-gray-50 hover:border-b-2 border-orange-300" onClick={handleMenuToggle}>Buffet</a></li>
+                        <li><a href="#services" className="text-orange-300 hover:text-gray-50 hover:border-b-2 border-orange-300" onClick={handleMenuToggle}>Serviços</a></li>
+                        <li><a href="#location" className="text-orange-300 hover:text-gray-50 hover:border-b-2 border-orange-300" onClick={handleMenuToggle}>Localização</a></li>
+                        <li><a href="#faleconosco" className="text-orange-300 hover:text-gray-50 hover:border-b-2 border-orange-300" onClick={handleMenuToggle}>Fale Conosco</a></li>
                     </ul>
                     <button className="md:hidden text-orange-300 border-r border-gray-300 pr-2" onClick={handleMenuToggle}>
-                        <FaBars size={24} />
+                        <FaBars size={35} />
                     </button>
                 </nav>
             </div>
