d81144e62e2c8bd1625cb00a2f6702ec6dc5a18f
JuniorPastorini <juniorpastorini_@hotmail.com>
2024-07-24 18:09:24 -0300

    remove modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import IntroModal from './sections/IntroModal';
+//import { useState } from 'react';
+//import IntroModal from './sections/IntroModal';
 import Header from './sections/Header';
 import HeroSection from './sections/HeroSection';
 import SpecialAndIfood from './sections/SpecialAndIfood';
@@ -9,25 +9,22 @@ import Location from './sections/Location';
 import Footer from './sections/Footer';
 
 function App() {
-  const [isIntroModalOpen, setIsIntroModalOpen] = useState(true);
+  //const [isIntroModalOpen, setIsIntroModalOpen] = useState(true);
 
-  const handleCloseModal = () => {
-    setIsIntroModalOpen(false);
-  };
+  //const handleCloseModal = () => {
+  //  setIsIntroModalOpen(false);
+  //};
   return (
     <>
-      {isIntroModalOpen && <IntroModal onClose={handleCloseModal} />}
-      {!isIntroModalOpen && (
-        <main className="flex min-h-screen flex-col">
-          <Header />
-          <HeroSection />
-          <SpecialAndIfood />
-          <Gallery />
-          <Services />
-          <Location />
-          <Footer />
-        </main>
-      )}
+      <main className="flex min-h-screen flex-col">
+        <Header />
+        <HeroSection />
+        <SpecialAndIfood />
+        <Gallery />
+        <Services />
+        <Location />
+        <Footer />
+      </main>
     </>
   );
 }
